8877eff1c9f90f55da8a9a0d6f5b2bd1116eb312
DCR1PPS <113095336+DCR1PPS@users.noreply.github.com>
2025-12-26 18:59:27 +0000

    Cycle RGB LEDs

--- sketch_dec26a/sketch_dec26a.ino
+++ sketch_dec26a/sketch_dec26a.ino
@@ -1,14 +1,28 @@
-#define LED 4
+#define LED_RED 4
+#define LED_GREEN 16
+#define LED_BLUE 17
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+  // Red
+  digitalWrite(LED_RED, LOW);
+  delay(1000);
+  digitalWrite(LED_RED, HIGH);
+  
+  // Green
+  digitalWrite(LED_GREEN, LOW);
+  delay(1000);
+  digitalWrite(LED_GREEN, HIGH);
+  
+  // Blue
+  digitalWrite(LED_BLUE, LOW);
+  delay(1000);
+  digitalWrite(LED_BLUE, HIGH);
 }
