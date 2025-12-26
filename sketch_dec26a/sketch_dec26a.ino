e9859a3b1ffcd0dd9a9dbefadd8eb8476fc68899
DCR1PPS <113095336+DCR1PPS@users.noreply.github.com>
2025-12-26 13:11:02 +0000

    Blink the screen

--- sketch_dec26a/sketch_dec26a.ino
+++ sketch_dec26a/sketch_dec26a.ino
@@ -1,9 +1,14 @@
+#define LED 4
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED, OUTPUT)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED, HIGH)
+  delay(500)
+  digitalWrite(LED, LOW)
+  delay(500)
 }
